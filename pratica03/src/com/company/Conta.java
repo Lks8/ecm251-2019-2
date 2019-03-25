05d52e55560578454674f574b297473782756fb0
LUCAS MARTINS COELHO <15.03863-7@maua.br>
2019-03-25 10:08:28 -0300

    Aula pratica 3

---
+++ pratica03/src/com/company/Conta.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Conta {
+    int numero;
+    double saldo;
+    String nome;
+}
---
+++ pratica03/src/com/company/ContaTestDrive.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class ContaTestDrive {
+    public static void main(String[] args) {
+        Conta c1;
+        c1 = new Conta();
+        c1.saldo = 1000;
+        System.out.println("Saldo da conta: " + c1.saldo);
+    }
+}
