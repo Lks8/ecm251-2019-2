ecab6baac6f559849fd8e30eb4ef45166911381b
LUCAS MARTINS COELHO <15.03863-7@maua.br>
2019-03-25 12:48:30 -0300

    Exemplo lojinha

--- pratica03/projetoCliente/src/com/company/Main.java
+++ pratica03/projetoCliente/src/com/company/Main.java
@@ -7,5 +7,7 @@ public class Main {
 	    Produto produto = new Produto(0,4.5,"Coxinha");
 	    Compra c1 = new Compra(1,cliente,produto);
         System.out.println(c1.pegarDetalhes());
+        Compra c2 = new Compra(2, new Cliente ("Roberto"), new Produto(1,4.5,"Café"));
+        System.out.println(c2.pegarDetalhes());
     }
 }
