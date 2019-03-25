8f0250a9d05e6ed7801820df6345eda2279b97a8
LUCAS MARTINS COELHO <15.03863-7@maua.br>
2019-03-25 11:49:36 -0300

    Aula 3 exemplo

--- pratica03/src/com/company/ContaTestDrive.java
+++ pratica03/src/com/company/ContaTestDrive.java
@@ -3,8 +3,17 @@ package com.company;
 public class ContaTestDrive {
     public static void main(String[] args) {
         Conta c1;
-        c1 = new Conta();
-        c1.saldo = 1000;
-        System.out.println("Saldo da conta: " + c1.saldo);
+        c1 = new Conta(0, "All might");
+        Conta c2 = new Conta(1,"Deku",500);
+        Conta c3 = new Conta(2,"Kazu",-800);
+        c1.depositar(1000);
+        System.out.println("Saldo da conta: " + c1.getSaldo());
+        c1.sacar(300);
+        c1.depositar(1000);
+        System.out.println("Saldo da conta: " + c1.getSaldo());
+        //System.out.println("Conta: " + c2.getNome() + " Saldo: " + c2.getSaldo());
+        //System.out.println("Conta: " + c3.getNome() + " Saldo: " + c3.getSaldo());
+        c2.exibirDetalhes();
+        c3.exibirDetalhes();
     }
 }
