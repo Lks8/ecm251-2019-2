6f12b7d77772d9c128aa2d15896fc67261e00335
LUCAS MARTINS COELHO <15.03863-7@maua.br>
2019-04-29 11:40:45 -0300

    Arrays

---
+++ ClassesAbstratas/src/com/company/Lobo.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Lobo extends Animal {
+    @Override
+    public void comer(){
+        System.out.println("Eu como comida de lobo!");
+    }
+}
--- ClassesAbstratas/src/com/company/main.java
+++ ClassesAbstratas/src/com/company/main.java
@@ -1,4 +1,24 @@
 package com.company;
 
-public class main {
+public class main{
+    public static void main(String[] args) {
+        Animal[] animais =new Animal[3];
+        animais[0]= new Lobo();
+        animais[0].comer();
+        animais[1] = new Lobo();
+        animais[2] = new Lobo();
+
+//        Animal a = new Lobo();
+//        Animal b = new Peixe();
+//        a.comer();
+//        b.comer();
+
+//       for(int i=0; i<animais.length; i++){
+//            animais[i].comer();
+//       }
+
+        for(Animal animal : animais){
+            animal.comer();
+        }
+    }
 }
