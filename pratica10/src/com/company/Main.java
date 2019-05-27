3cfaceb5125825f0f6aad8e23c6e9034f9827fbc
LUCAS MARTINS COELHO <15.03863-7@maua.br>
2019-05-27 10:13:44 -0300

    pratica 10

---
+++ pratica10/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Cliente c1 = new Cliente ("Teste 1");
+        System.out.println(c1);
+        personagens.Cliente c2 = new personagens.Cliente("Teste 2");
+    }
+}
