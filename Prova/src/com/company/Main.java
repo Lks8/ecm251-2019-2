67ddc7a6b64e2ab6927c2676abbb23c81463bf48
LUCAS MARTINS COELHO <15.03863-7@maua.br>
2019-06-24 10:15:10 -0300

    Atividade2

---
+++ Prova/src/com/company/Main.java
@@ -0,0 +1,10 @@
+// Lucas Martins Coelho 15.03863-7
+// Gustavo Villela Rubia 17.01736-0
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
