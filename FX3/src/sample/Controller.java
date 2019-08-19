250d3f7e1130e80ef6c9c21c31830b667fce0133
LUCAS MARTINS COELHO <15.03863-7@maua.br>
2019-08-19 12:37:38 -0300

    FX

--- FX3/src/sample/Controller.java
+++ FX3/src/sample/Controller.java
@@ -1,4 +1,32 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.BasicCalculator;
+
 public class Controller {
+    @FXML
+    private TextField txtValue1;
+    @FXML
+    private TextField txtValue2;
+    @FXML
+    private Label lblRes;
+    @FXML
+    public void sum(){
+        BasicCalculator cassio = new BasicCalculator();
+        lblRes.setText(""+cassio.getAdd(txtValue1.getText() , txtValue2.getText()));
+    }
+    public void sub(){
+        BasicCalculator cassio = new BasicCalculator();
+        lblRes.setText(""+cassio.getSub(txtValue1.getText() , txtValue2.getText()));
+    }
+    public void multi(){
+        BasicCalculator cassio = new BasicCalculator();
+        lblRes.setText(""+cassio.getMulti(txtValue1.getText() , txtValue2.getText()));
+    }
+    public void div(){
+        BasicCalculator cassio = new BasicCalculator();
+        lblRes.setText(""+cassio.getDiv(txtValue1.getText() , txtValue2.getText()));
+    }
 }
