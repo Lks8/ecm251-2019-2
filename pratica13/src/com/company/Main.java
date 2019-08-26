068f0e42b2067354aa4aaaf5ab53b6f95ad61b9f
LUCAS MARTINS COELHO <15.03863-7@maua.br>
2019-08-26 12:59:07 -0300

    banco de dados

--- pratica13/src/com/company/Main.java
+++ pratica13/src/com/company/Main.java
@@ -1,18 +1,22 @@
 package com.company;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import auxiliar_database.UsuariosDAO;
+import model.Usuario;
+import utilities.Constants;
+
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection("jdbc:sqlite:meu_banco.db");
-        } catch (SQLException e ){
-            System.out.println("Erro ao conectar:");
-            e.printStackTrace();
+        Aplicacao app = new Aplicacao();
+
+        UsuariosDAO usuariosDAO;
+        usuariosDAO = new UsuariosDAO(Constants.URL_MEU_BANCO);
+        List<Usuario> meusUsuarios = usuariosDAO.getAllUsuario();
+        for (Usuario user: meusUsuarios){
+            System.out.print("ID: " + user.id);
+            System.out.println("Nome: " + user.nome);
         }
     }
 }
